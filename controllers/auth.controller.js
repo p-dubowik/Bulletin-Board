5afc51a36025b7c111fef02afcc66cf687ae0ccb
Paweł Dubowik <p.dubowik27@gmail.com>
2026-04-12 20:26:36 +0200

    Ad multer package and make use of it in imageupload. Add imageUpload as middleware in register endpoint. Create getImageFileType util function to check file header for correct file formats. Change register endpoint to check for correct files upon registration

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,17 +1,22 @@
 const User = require('../models/User.model');
 const Session = require('../models/Session.model');
 const bcrypt = require('bcryptjs');
+const getImageFileType = require('../utils/getImageFileType');
 
 exports.register = async (req, res) => {
     try {
-        const { login, password } = req.body;
-            if(login && typeof login === 'string' && password && typeof password === 'string') {
+
+        const { login, password, number } = req.body;
+        const fileType = req.file ? await getImageFileType(req.file) : unknown;
+
+
+            if(login && typeof login === 'string' && password && typeof password === 'string' && req.file && ['image/png', 'image/jpeg', 'image/gif'].includes(filetype)) {
                 const userWithLogin = await User.findOne({ login });
                 if(userWithLogin){
                     return res.status(409).send({message: 'User already exists'});
                 }
 
-                const user = await User.create({ login, password: await bcrypt.hash(password, 10) });
+                const user = await User.create({ login, password: await bcrypt.hash(password, 10), number: number, avatar: req.file.filename });
                 res.status(201).send({ message: 'User created ' + user.login});
             } else {
                 res.status(400).send({ message: 'Bad request'});
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const authMiddleware = require('../utils/authMiddleware');
 
 const auth = require('../controllers/auth.controller');
+const imageUpload = require('../utils/imageUpload');
 
 
-router.post('/register', auth.register);
+router.post('/register', imageUpload.single('avatar'), auth.register);
 
 router.post('/login', auth.login);
 
